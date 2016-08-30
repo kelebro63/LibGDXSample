34b6a0b4bb2c22365efde2c122f69885f1b9747e
Kelebro63 <kelebro63@gmail.com>
2016-08-30 09:42:37 +0300

    part3

--- LibGDX/core/src/kelebro63/com/mainGame.java
+++ LibGDX/core/src/kelebro63/com/mainGame.java
@@ -9,9 +9,14 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
-import java.awt.Rectangle;
+import java.util.Iterator;
+
 
 public class mainGame extends ApplicationAdapter {
 	OrthographicCamera camera;
@@ -22,6 +27,8 @@ public class mainGame extends ApplicationAdapter {
 	Music rainMusic;
 	Rectangle bucket;
 	Vector3 touchPos;
+	Array<com.badlogic.gdx.math.Rectangle> raindrops;
+	long lastDropTime;
 
 	@Override
 	public void create () {
@@ -46,6 +53,19 @@ public class mainGame extends ApplicationAdapter {
 		bucket.height = 64;
 
 		touchPos = new Vector3();
+
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(800 - 64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
 	}
 
 	@Override
@@ -58,6 +78,9 @@ public class mainGame extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for (Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
 
 		if (Gdx.input.isTouched()) {
@@ -66,11 +89,28 @@ public class mainGame extends ApplicationAdapter {
 			bucket.x = (int) (touchPos.x - 64 / 2);
 		}
 
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 2000 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
 
 		if (bucket.x < 0) bucket.x = 0;
 		if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+			spawnRaindrop();
+		}
+
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while (iter.hasNext()) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + 64 < 0) {
+				iter.remove();
+			}
+			if (raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
 	}
 	
 	@Override
@@ -78,6 +118,8 @@ public class mainGame extends ApplicationAdapter {
 		batch.dispose();
 		dropImage.dispose();
 		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
 	}
 
 	@Override
